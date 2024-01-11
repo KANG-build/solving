0711b2c78adc09770891aeb02004771e998240e0
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-11 17:51:32 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,17 +1,12 @@
 def solution(keyinput, board):
     answer = [0, 0]
-    
     for i in keyinput:
         if i == "up":
-            if answer[1] < (board[1]//2):
-                answer[1] += 1
+            answer[1] = min(answer[1] + 1, (board[1]//2))
         elif i == "down":
-            if answer[1] > -(board[1]//2): 
-                answer[1] -= 1
+            answer[1] = max(answer[1] - 1, -(board[1]//2))
         elif i == "left":
-            if answer[0] > -(board[0]//2):
-                answer[0] -= 1
+            answer[0] = max(answer[0] - 1, -(board[0]//2))
         elif i == 'right':
-            if answer[0] < (board[0]//2):
-                answer[0] += 1
+            answer[0] = min(answer[0] + 1, (board[0]//2))
     return answer
