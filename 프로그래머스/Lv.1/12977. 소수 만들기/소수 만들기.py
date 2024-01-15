8d8d6f9c693708f43d45d5e661aefb71ca2a228e
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-15 15:17:25 +0900

    [level Lv.1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/Lv.1/12977. 소수 만들기/소수 만들기.py
@@ -4,10 +4,7 @@ def solution(nums):
     for num in [nums[i]+nums[j]+nums[k] for i in range(len(nums)) for j in range(i+1, len(nums)) for k in range(j+1, len(nums))]:
         for div in range(2, num):
             if num % div == 0:
-                check = 1
                 break
-
-        if check == 0:
-            count += 1
-        else: check = 0         
+        else:
+                count += 1
     return count 
