9b04fb16a1ef940bf4ecf2a4a29c29d167ae1f92
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-15 14:18:23 +0900

    [level Lv.1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/Lv.1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/Lv.1/135808. 과일 장수/과일 장수.py
@@ -1,4 +1,2 @@
 def solution(k, m, score):
-    apple = sorted(score, reverse=True)
-    appleBox = [apple[i:i+m] for i in range(0, len(apple), m)]
-    return sum(min(i) * m for i in appleBox if len(i) == m)
+    return sum(sorted(score)[len(score)%m::m] * m)
