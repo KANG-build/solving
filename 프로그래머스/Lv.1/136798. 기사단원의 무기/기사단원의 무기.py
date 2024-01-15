5b5daa7389a16af909c1f7812a8c410ab7edd3f3
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-15 14:45:48 +0900

    [level Lv.1] Title: 기사단원의 무기, Time: 3.83 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,15 @@
+def solution(number, limit, power):
+    hap = 0
+    steel = 0
+    for i in range(1, number+1):
+        # 제곱근 n까지만 계산해서 시간을 줄인다
+        for j in range(1, int((i)**0.5)+1):
+            if j == (i**0.5):
+                hap += 1
+            elif i % j == 0:
+                hap += 2
+        if hap > limit:
+            steel += power
+        else: steel += hap
+        hap = 0
+    return steel
