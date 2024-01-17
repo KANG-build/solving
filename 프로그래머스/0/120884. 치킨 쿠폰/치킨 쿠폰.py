adac30f4eaeb108aef591315929c4409a3faa198
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-17 18:07:01 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,12 +1,2 @@
 def solution(chicken):
-    result = 0
-    ticket = 0
-    while True:
-        result += chicken // 10
-        ticket += chicken % 10
-        chicken = chicken // 10
-        
-        if chicken < 10:
-            result += (chicken+ticket)//10
-            result +=  ((chicken+ticket)%10 + (chicken+ticket)//10)//10
-            return result 
+    return int(chicken*0.111111111)
