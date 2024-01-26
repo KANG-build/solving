9a0e9d1a4917f12abe11643f757a786af6d59fb5
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-26 20:42:08 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,18 +1,3 @@
 def solution(my_string):
-    box = []
-    keep = []
-    for idx, i in enumerate(my_string):
-        if i.isdigit():  # 숫자라면
-            string = ''
-            while my_string[idx].isdigit():
-                if idx not in keep:
-                    string += my_string[idx]
-                keep.append(idx)
-                idx += 1
-                
-                if idx >= len(my_string):
-                    break
-                
-            if string:
-                box.append(int(string))
-    return sum(box)
+    s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+    return sum(int(i) for i in s.split())
