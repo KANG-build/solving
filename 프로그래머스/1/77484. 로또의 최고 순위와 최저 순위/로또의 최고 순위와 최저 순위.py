f5e95c3155b9816478c8f2ae958d42b34acc6003
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-26 20:55:42 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,10 +1,9 @@
 def solution(lottos, win_nums):
-    cor = []
+    cor = 0
     for lotto in lottos:
-        for win_num in win_nums:
-            if lotto == win_num:
-                cor.append(lotto)
+        if lotto in win_nums:
+            cor += 1
     if 0 in lottos or cor:
-        return [max(1, 7-lottos.count(0)-len(cor)), min(6, 7-len(cor))]
+        return [max(1, 7-lottos.count(0)-cor), min(6, 7-cor)]
     else:
         return [6, 6]
