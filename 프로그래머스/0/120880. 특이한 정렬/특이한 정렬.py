ad1c155f105ca97b1605b2b9ee8c84b28393c662
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-31 10:12:40 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,14 +1,2 @@
 def solution(numlist, n):
-    temp = 0
-    for i in range(len(numlist)-1):
-        for j in range(len(numlist)-1):
-            if abs(numlist[j]-n) > abs(numlist[j+1]-n):
-                temp = numlist[j]
-                numlist[j] = numlist[j+1]
-                numlist[j+1] = temp
-            if abs(numlist[j]-n) == abs(numlist[j+1]-n):
-                if numlist[j] < numlist[j+1]:
-                    temp = numlist[j]
-                    numlist[j] = numlist[j+1]
-                    numlist[j+1] = temp
-    return numlist
+    return sorted(numlist, key = lambda x : (abs(x-n), n-x))
