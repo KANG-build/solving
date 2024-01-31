b4c6e3a17ca6c9b06586a3f7f1e154c92a118ee8
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-01-31 12:14:33 +0900

    [level 1] Title: 문자열 나누기, Time: 0.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,19 +1,11 @@
 def solution(s):
-    answer = 0
-    while s:
-        x = s[0]
-        idx, right, wrong = 0, 0, 0
-        for i in range(len(s)):
-            if s[i] != x:
-                wrong += 1
-            else: 
-                right += 1
-            if right == wrong:
-                answer += 1
-                idx = i
-                break
-        else:
+    answer, right, wrong = 0, 0, 0
+    for i in s:
+        if right == wrong:
             answer += 1
-            break
-        s = s[idx+1:]
+            a = i
+        if i == a:
+            right += 1
+        else:
+            wrong += 1
     return answer
