74a5317cd26b297c7789793336743aceb69d1514
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-02-02 11:05:15 +0900

    [level 1] Title: 소수 찾기, Time: 317.76 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,9 +1,6 @@
 def solution(n):
-    count = 0
+    num = set(range(2, n+1))
     for i in range(2, n+1):
-        for j in range(2, int(i**0.5+1)):
-            if i % j == 0:
-                break
-        else:
-            count += 1
-    return count
+        if i in num:
+            num -= set(range(2*i, n+1, i))
+    return len(num)
