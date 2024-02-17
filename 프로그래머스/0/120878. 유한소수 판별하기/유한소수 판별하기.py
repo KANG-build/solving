52c5ef84747e92e8a8800e7415ca0565a3d1d5ff
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-02-17 17:50:00 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,16 +1,12 @@
 import math as m
 def solution(a, b):
-    factors = []
-    
-    # 기약분수화
+
     b //= m.gcd(a, b)
-    i = 2
-    # 분모의 소인수 판별
-    while i <= b:
-        if b % i == 0:
-            factors.append(i)
-            b /= i
-        else: i += 1
-    if (set(factors) - set([2, 5])):
-        return 2
-    return 1
+    
+    while b%2 == 0:
+        b //= 2
+    while b%5 == 0:
+        b //= 5
+    if b == 1:
+        return 1
+    return 2
