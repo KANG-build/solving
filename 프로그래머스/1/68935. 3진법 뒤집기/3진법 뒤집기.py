1d201d501f1f7a4096ec8cfd2a77c9917222a0e4
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-02-28 14:29:59 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,16 +1,8 @@
 def solution(n):
-    a = 1
-    num = ''
-    answer = 0
+    tmp = ''
 
-    while 3**a <= n: 
-        a += 1
-    
-    for i in range(1, a+1):
-        num += str(n//3**(a-i))
-        n -= 3**(a-i)*(n//3**(a-i))
+    while n:
+        tmp += str(n%3)
+        n = n//3
         
-    for idx, s in enumerate(num):
-        answer += int(s) * 3**idx
-        
-    return answer
+    return int(tmp, 3)
