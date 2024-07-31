537e863b0a4c41760f9ea774105bc5b7e8b74089
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-07-31 14:13:47 +0900

    [Silver V] Title: 숫자 카드, Time: 840 ms, Memory: 131324 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,12 @@
+N = int(input())  # 상근이가 가지고 있는 숫자 카드의 개수
+num = map(int, input().split())  # 숫자 카드에 적혀있는 정수
+M = int(input())  # M개의 정수를 받겟다
+IN = list(map(int, input().split()))  # 상근이가 가지고 있는지 구해야하는 정수
+dic = dict(zip(IN, [0 for i in range(len(IN))]))
+
+for i in num:
+    if i in dic:
+        dic[i] = 1
+
+for i in range(M):
+    print(dic[IN[i]], end=' ')
