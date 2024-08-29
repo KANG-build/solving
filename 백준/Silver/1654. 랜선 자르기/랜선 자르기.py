3d0d3f4d65fee5e033948d4bd2d4258eef7e12f8
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-08-30 02:48:11 +0900

    [Silver II] Title: 랜선 자르기, Time: 312 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,33 @@
+#####################
+# 1654. 랜선 자르기 #
+#####################
+
+# 남는 길이가 mid * m이면 된다
+n, m = map(int, input().split())
+array = []
+
+for i in range(n):
+    array.append(int(input()))
+
+# start, end
+start = 1
+end = max(array)
+
+# 이진탐색 구현
+result = 0
+while start <= end:
+    mid = (start + end) // 2
+    # 잘린 랜선 체크
+    total = 0
+    for i in array:
+        if i >= mid:
+            total += i//mid  # 잘라서 몇개가 나오는지
+    
+    # 왼쪽 확인 (랜선 부족)
+    if total < m:  # 개수 부족하면 다시
+        end = mid-1
+    else:
+        result = mid
+        start = mid+1
+        
+print(result)
