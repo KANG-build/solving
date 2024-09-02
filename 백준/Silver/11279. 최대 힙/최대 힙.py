b0d0e7db09597940dd09ddfb0c3a33df74a6e4a2
MOL <127071229+yangsieunn@users.noreply.github.com>
2024-09-02 22:09:33 +0900

    [Silver II] Title: 최대 힙, Time: 104 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,23 @@
+##################
+# 11279. 최대 힙 #
+##################
+
+import heapq
+import sys
+input = sys.stdin.readline
+
+
+heap = []
+n = int(input())
+for i in range(n):
+    x = int(input())
+    if x != 0:
+        # 배열에 x라는 값을 넣는 연산
+        heapq.heappush(heap, -x)
+    else:
+        # 배열에서 가장작은 값을 출력하고 그 값을 배열에서 제거
+        # 비어있으면 0 출력
+        if heap:
+            print(-(heapq.heappop(heap)))
+        else:
+            print(0)
