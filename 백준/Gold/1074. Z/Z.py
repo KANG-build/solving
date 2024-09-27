5029e43776c0c7f52c7db8e65b317768a9d03970
yang sieun <127071229+KANG-build@users.noreply.github.com>
2024-09-28 01:30:18 +0900

    [Gold V] Title: Z, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -4,13 +4,20 @@ sys.setrecursionlimit(100000)
 # 좌표로 체크
 def visit(x1, y1, x2, y2):
     global count
+    global answer
     
     if x2 == x1+2 or y2 == y1+2:  # 2*2일때
-        if x1 <= c <= x2 and y1 <= r <= y2:
-            check(x1, y1, x2, y2)
-            return
-        else:
-            count += 4
+        # (r, c)면 count 출력 후 종료
+        if x1 == c and y1 == r:
+            answer = count
+        elif c == (x1+1) and y1 == r:
+            answer = count+1
+        elif x1 == c and r == (y1+1):
+            answer = count+2
+        elif c == (x1+1) and r == (y1+1):
+            answer = count+3  
+        return
+    
     else:
         # 왼쪽위 방문
         if x1 <= c < x1+(x2-x1)//2 and y1 <= r < y1+(y2-y1)//2: 
@@ -36,21 +43,6 @@ def visit(x1, y1, x2, y2):
         else:
             count += (x2-x1)//2 * (y2-y1)//2
 
-    return
-    
-def check(x1, y1, x2, y2):
-    global count
-    global answer
-    
-    # (r, c)면 count 출력 후 종료
-    if x1 == c and y1 == r:
-        answer = count
-    elif c == (x1+1) and y1 == r:
-        answer = count+1
-    elif x1 == c and r == (y1+1):
-        answer = count+2
-    elif c == (x1+1) and r == (y1+1):
-        answer = count+3  
     return
     
 n, r, c = map(int, input().split())
