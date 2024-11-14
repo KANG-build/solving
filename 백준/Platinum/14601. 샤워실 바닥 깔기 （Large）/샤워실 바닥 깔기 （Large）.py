76ebcba315fee375bab05d04afeb206d591b7130
yang sieun <127071229+KANG-build@users.noreply.github.com>
2024-11-14 20:08:14 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.py
+++ 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.py
@@ -1,94 +1,93 @@
+# board : 이차원 리스트
+# prow, pcol : 현재 분할이 위치한 좌표
+# size : 현재 분할의 크기
+# xrow, xcol : 배수구가 있는 부분의 좌표
+
+# n : 현재의 사분면
+# 현재 좌표를 제외한 나머지 사분면을 칠하는 함수
+
 import sys
 input = sys.stdin.readline
 
-# 트로미노 함수
-# board : 이차원 리스트
-# start_row, start_col : 현재 분할의 시작 좌표
-# size : 현재 분할의 크기
-# empty_row, empty_col : 비어 있는 곳의 좌표
-
-
-def tromino(board, start_row, start_col, size, empty_row, empty_col):
-    if size == 1:
+def fillCenterExcept(board,size,prow,pcol,n):
+    global count
+    mrow=prow+(size//2)
+    mcol=pcol+(size//2) 
+    if n == 1:
+      board[mrow-1][mcol-1] = count
+      board[mrow][mcol-1] = count
+      board[mrow][mcol] = count
+    elif n == 2:
+      board[mrow-1][mcol] = count
+      board[mrow][mcol-1] = count
+      board[mrow][mcol] = count    
+    elif n == 3:
+      board[mrow-1][mcol] = count
+      board[mrow-1][mcol-1] = count
+      board[mrow][mcol] = count    
+    elif n == 4:
+      board[mrow-1][mcol] = count
+      board[mrow-1][mcol-1] = count
+      board[mrow][mcol-1] = count
+    count += 1
+    
+#트로미노가 들어갈 좌표를 찾아서 분할 재귀하는 함수
+def tromino(board,size,prow,pcol,xrow,xcol):
+    if size==1:
         return
+    
+    else:
+        mrow=prow+(size//2)
+        mcol=pcol+(size//2)
+
+        xrow1,xcol1=mrow-1,mcol 
+        xrow2,xcol2=mrow-1,mcol-1
+        xrow3,xcol3=mrow,mcol-1
+        xrow4,xcol4=mrow,mcol
+
+        if xrow<mrow and xcol>=mcol:
+           fillCenterExcept(board,size,prow,pcol,1)
+           xrow1, xcol1 = xrow, xcol
+        elif xrow<mrow and xcol<mcol:
+           fillCenterExcept(board,size,prow,pcol,2)
+           xrow2, xcol2 = xrow, xcol
+        elif xrow>=mrow and xcol<mcol:
+           fillCenterExcept(board,size,prow,pcol,3)
+           xrow3, xcol3 = xrow, xcol
+        elif xrow>=mrow and xcol>=mcol:
+           fillCenterExcept(board,size,prow,pcol,4)
+           xrow4, xcol4 = xrow, xcol
+            
+        tromino(board,size//2,prow,mcol,xrow1,xcol1)
+        tromino(board,size//2,prow,pcol,xrow2,xcol2)
+        tromino(board,size//2,mrow,pcol,xrow3,xcol3)
+        tromino(board,size//2,mrow,mcol,xrow4,xcol4)
         
-    middle_row = start_row + size // 2 # 중간 row
-    middle_col = start_col + size // 2 # 중간 col
+#화면에 트로미노를 출력하는 함수
+def printscr(board):                                          
+    for row in board: 
+        sys.stdout.write(" ".join(map(str, row)))
+        sys.stdout.write("\n")
+        
 
-    # 1 : 왼쪽 위
-    # 2 : 왼쪽 아래
-    # 3 : 오른쪽 위
-    # 4 : 오른쪽 아래
-    quad1_row, quad1_col = middle_row - 1, middle_col - 1
-    quad2_row, quad2_col = middle_row - 1, middle_col
-    quad3_row, quad3_col = middle_row, middle_col - 1
-    quad4_row, quad4_col = middle_row, middle_col
+#입력
+K=int(input())
+x,y=list(map(int,input().split()))
 
-    # 왼쪽 위에 빈칸 존재
-    if empty_row < middle_row and empty_col < middle_col:
-        fill(board, middle_row, middle_col, 1)
-        quad1_row, quad1_col = empty_row, empty_col
-    # 왼쪽 아래에 빈칸 존재
-    elif (empty_row < middle_row and empty_col >= middle_col):
-        fill(board, middle_row, middle_col, 2)
-        quad2_row, quad2_col = empty_row, empty_col
-    # 오른쪽 위에 빈칸 존재
-    elif (empty_row >= middle_row and empty_col < middle_col):
-        fill(board, middle_row, middle_col, 3)
-        quad3_row, quad3_col = empty_row, empty_col
-    # 오른쪽 아래에 빈칸 존재
-    elif (empty_row >= middle_row and empty_col >= middle_col):
-        fill(board, middle_row, middle_col, 4)
-        quad4_row, quad4_col = empty_row, empty_col
+#왼쪽 아래가 (1,1) 좌표이므로 변환이 이루어짐
+full=2**K
+px=full-y
+py=x-1
 
-    # 재귀 호출 : 분할
-    tromino(board, start_row, start_col, size // 2, quad1_row, quad1_col)
-    tromino(board, start_row, middle_col, size // 2, quad2_row, quad2_col)
-    tromino(board, middle_row, start_col, size // 2, quad3_row, quad3_col)
-    tromino(board, middle_row, middle_col, size // 2, quad4_row, quad4_col)
+#보드판 생성하기
+board = [[0]*(2**K) for _ in range(2**K)]
+board[px][py]=-1
 
+#트로미노 타일링을 위한 카운터 초기화
+count = 1
 
+#트로미노 실행
+tromino(board, 2**K, 0, 0, px, py)
 
-
-
-
-
-# 각 카운트를 통해 어떤 트로미노가 들어갔는지 기록
-# part : 현재의 사분면
-def fill(board, mrow, mcol, part):
-    global tromino_count
-    tromino_count += 1
-    if (part != 1):
-        board[mrow - 1][mcol - 1] = tromino_count
-    if (part != 2):
-        board[mrow - 1][mcol] = tromino_count
-    if (part != 3):
-        board[mrow][mcol - 1] = tromino_count
-    if (part != 4):
-        board[mrow][mcol] = tromino_count
-
-# 원점을 재정의 
-def XYsetting(x, y):
-    new_x = n - y
-    new_y = x - 1
-    return (new_x, new_y)
-
-# 입력
-k = int(input())
-x, y = map(int, input().split())
-
-# 왼쪽 아래가 (1,1)이므로 변환
-n = 2 ** k
-new_x, new_y = XYsetting(x, y)
-
-# 보드판 생성
-board = [[0] * (2 ** k) for _ in range(2 ** k)]
-board[new_x][new_y] = -1
-
-# 트로미노 함수 실행
-tromino_count = 0
-tromino(board, 0, 0, 2 ** k, new_x, new_y)
-
-# 출력
-for i in board:
-    print(*i)
+#화면에 출력
+printscr(board)
