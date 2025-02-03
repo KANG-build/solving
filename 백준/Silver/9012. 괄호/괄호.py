7d482b8e5328142a97a948af049c12b8f5666346
yang sieun <127071229+KANG-build@users.noreply.github.com>
2025-02-03 21:02:50 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,12 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-a = int(input())
-for i in range(a):
-    b = input()
-    b = b.replace(')', '),')
-    try:
-        eval(b)
-        print('YES')
-    except:
-        print('NO')
+n = int(input())
+
+for i in range(n):
+    string = list(input()) # 문자열을 list화 하면 모든 문자가 다 분리된다.
+    check = 0
+    stack = []
+
+    for i in string:
+        if i == '(':
+            stack.append('(')
+        if i == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else: 
+                check = 1
+                break
+    if check == 1:
+        print("NO")
+    else:
+        if stack:
+            print("NO")
+        else: print("YES")
