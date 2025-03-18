7e4ceb471a91088738918f0280c437bc5ce4c2cf
yang sieun <127071229+KANG-build@users.noreply.github.com>
2025-03-18 20:24:54 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,5 +1,17 @@
 def solution(n, words):
+    # 시작 알파벳 별로 딕셔너리 만들어서 거기서 찾으면 빠를듯?
+    # 틀린 단어부터 찾고 사람을 구하자 
+    set_words = set([words[0]])
+    wrong_num = 0
     for i in range(1, len(words)):
-        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
-            return [i%n+1, i//n+1]
+        if words[i][0] != words[i-1][-1]:
+            wrong_num = i
+            return [wrong_num%n+1, wrong_num//n+1]
+        
+        if words[i] in set_words:
+            wrong_num = i
+            return [wrong_num%n+1, wrong_num//n+1]
+        
+        set_words.add(words[i])
+
     return [0, 0]
